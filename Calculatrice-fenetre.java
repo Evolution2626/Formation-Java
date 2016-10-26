ccf2171e05f4afbe264c4bbce7495ec18ca94c04
TheAmo <apalaosds@gmail.com>
2016-10-26 19:19:51 -0400

    Update Calculatrice-fenetre.java

--- Calculatrice-fenetre.java
+++ Calculatrice-fenetre.java
@@ -1,10 +1,54 @@
 package formation.reponse
 
-import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.*;
  
-public class Calculatrice-fenetre {
-  public static void main(String[] args){
-    JFrame fenetre = new JFrame();
-    fenetre.setVisible(true);
-  }       
+public class Calculatricefenetre extends JFrame{
+ 
+    JButtonExample(){  
+        /* JFrame is a top level container (window)
+         * where we would be adding our button
+         */
+        JFrame frame=new JFrame();  
+                          
+        // Creating Button  
+       JButton showDialogButton = new JButton("7");
+       showDialogButton.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent e){
+          // display/center the jdialog when the button is pressed
+          chiffre = 7;
+        }
+       });        
+      JButton showDialogButton = new JButton("8");
+       showDialogButton.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent e){
+          // display/center the jdialog when the button is pressed
+          chiffre = 8;
+        }
+       }); 
+      JButton showDialogButton = new JButton("9");
+       showDialogButton.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent e){
+          // display/center the jdialog when the button is pressed
+          chiffre = 9;
+        }
+       }); 
+             
+        //Adding button onto the frame
+        frame.add(b);  
+                  
+        // Setting Frame size. This is the window size
+        frame.setSize(300,200);  
+        
+        frame.setLayout(null);  
+        frame.setVisible(true);  
+                  
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+    }  
+
+ 
+              
+     public static void main(String[] args) {  
+            new JButtonExample();  
+     }  
 }
