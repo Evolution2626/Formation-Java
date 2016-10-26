9439ce5df0e87d6dd14d6d744fef65bb4c84c59c
TheAmo <apalaosds@gmail.com>
2016-10-26 17:23:28 -0400

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -5,13 +5,24 @@
 * 1 4 6 4 1
 */
 
-//1,1,2,3,5,8,13,21,...
+//0,1,1,2,3,5,8,13,21,...
 
 package formation.reponse
 
 public class {
 
   public static void main(String[] args){
-  
+    double premier = 0;
+    double deuxieme = 1;
+    double resultat = 0;
+    
+    while(true){
+      resultat = premier + deuxieme;//calcule le resultat
+      
+      System.out.println(resultat); //imprimer le resultat
+      
+      premier = deuxieme;//mettre a niveau les chiffres
+      deuxieme = resultat;
+    }
   }
 }
