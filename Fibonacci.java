f76855768215caea7b24c6c05a6fe6385d7fc6fb
TheAmo <apalaosds@gmail.com>
2016-10-30 18:56:30 -0400

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -7,22 +7,33 @@
 
 //0,1,1,2,3,5,8,13,21,...
 
-package formation.reponse
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package formation;
 
+/**
+ *
+ * @author TheAmo
+ */
 public class Fibonacci {
 
-  public static void main(String[] args){
-    double premier = 0;
-    double deuxieme = 1;
-    double resultat = 0;
-    
-    while(true){
-      resultat = premier + deuxieme;//calcule le resultat
-      
-      System.out.println(resultat); //imprimer le resultat
-      
-      premier = deuxieme;//mettre a niveau les chiffres
-      deuxieme = resultat;
+    public static void main(String[] args) {
+        double premier = 0;
+        double deuxieme = 1;
+        double resultat = 0;
+
+        while (true) {
+            System.out.println(resultat); //imprimer le resultat
+            resultat = premier + deuxieme;//calcule le resultat
+
+            premier = deuxieme;//mettre a niveau les chiffres
+            deuxieme = resultat;
+            if(resultat > 1.3069892237633987E308 ){ //limite theorique du double. arrete pour pas avoir infini
+                System.exit(1);                         
+            }
+        }
     }
-  }
 }
